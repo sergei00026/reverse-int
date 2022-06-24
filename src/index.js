a6491e5c86a988ac4bf50e1c7f40a55c9a8dea2a
sergei00026 <sergei57fx@mail.ru>
2022-06-24 11:04:43 +0500

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	if (n > 0) {
+		return n.toString().split('').reverse().join('');
+		// a.split('');
+	} else {
+		return (`${-n}`.toString().split('').reverse().join(''));
+
+	}
 }
+
